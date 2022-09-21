5d49255157dce95b75acb1f4792fb1ed5b32b1a6
Ville_Immonen <ville.immonen86@gmail.com>
2022-09-21 14:09:29 +0300

    poistettiin kommentoidut koodit

--- weather_app/main.py
+++ weather_app/main.py
@@ -1,16 +1,6 @@
 from flask import Flask, render_template
-# from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
-# socketio = SocketIO(app, cors_allowed_origins="*")
-
-# @socketio.on("message") # pitää olla message muuten ei toimi
-# # vaihtoehdot parametreiksi:
-# # message, connect, disconnect, json
-# def handle_message(msg):
-#     print(msg)
-#     if msg != "User connected":
-#         send(msg, broadcast=True)
 
 @app.route("/")
 def index():
@@ -22,4 +12,3 @@ def weather_page():
 
 if __name__ == "__main__":
     app.run(debug=True)
-    # socketio.run(app, host="localhost")
